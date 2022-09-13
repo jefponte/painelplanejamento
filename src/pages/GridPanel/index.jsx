15dc65b7e4c701a0d93066f616978e523cff9e27
Jefferson Uchôa Ponte <jefponte@gmail.com>
2022-09-13 09:16:41 -0300

    Fetch data and show in GridPanel

--- src/pages/GridPanel/index.jsx
+++ src/pages/GridPanel/index.jsx
@@ -1,33 +1,51 @@
 import React, { useEffect, useState } from "react";
 import Box from '@mui/material/Box';
 import { DataGrid, ptBR } from '@mui/x-data-grid';
-import { useDemoData } from '@mui/x-data-grid-generator';
-import {
-  getData
-} from "../../services/api";
+
+
+
+const columns = [
+  {  field: 'id', headerName: "Nº", width: 25},
+  {  field: 'classificacaoObjeto', headerName: "Classificação do Objeto", width: 200},
+  {  field: 'objetivo', headerName: "Objetivo", width: 200}, 
+  {  field: 'classificacaoIndicador', headerName: "Classificação do Indicador", width: 250}, 
+  {  field: 'categoriaIndicador', headerName: "Categoria do Indicador"},
+  {  field: 'tipo', headerName: "Tipo de Indicador"}, 
+  {  field: 'descricao', headerName: "Descrição", width: 300}, 
+  {  field: 'percentual', headerName: "Percentual", width: 300}, 
+  {  field: 'descricaoMeta', headerName: "Descrição da Meta", width: 300}, 
+  {  field: 'prazo', headerName: "Prazo", width: 300}, 
+  {  field: 'unidadeResponsavel', headerName: "Unidade Responsavel", width: 300}, 
+  {  field: 'unidadeCoResponsavel', headerName: "Unidade Co-Responsavel", width: 300}
+];
 
 
 export default function GridPanel() {
-  const { data } = useDemoData({
-    dataSet: 'Commodity',
-    rowLength: 100000,
-    editable: false,
-  });
-  const [dataContent, setDataContent] = useState({});
+  const [goals, setGoals] = useState([]);
+  
+  const fetchGoals = async () => {
+    try {
+      const res = await fetch('https://sheetdb.io/api/v1/bj9pvb123v2kw');
+      const data = await res.json();
+      setGoals(data);
+    } catch(error) {
+
+    }
+  }
   useEffect(() => {
-    getData(setDataContent);
+    fetchGoals()
   }, []);
 
-  
-  console.log(data);
-  console.log(dataContent);
+
+
   return (
     <Box sx={{ height: 520, width: '100%' }}>
       <DataGrid
-        {...data}
-        loading={data.rows.length === 0}
+        rows={goals}
+        columns = {columns}
+        loading={goals.length === 0}
         rowHeight={40}
-         localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
+        localeText={ptBR.components.MuiDataGrid.defaultProps.localeText}
       />
     </Box>
   );
