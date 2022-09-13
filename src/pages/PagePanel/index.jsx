4111792dcee992e1012c4476e649131cd5779672
Jefferson Uchôa Ponte <jefponte@gmail.com>
2022-09-13 14:59:55 -0300

    Card logo

--- src/pages/PagePanel/index.jsx
+++ src/pages/PagePanel/index.jsx
@@ -2,28 +2,44 @@ import { Container, Typography } from "@mui/material";
 import React from "react";
 import GridPanel from "../GridPanel";
 import imagem from "../../assets/img/logo-proplan.png";
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+
 
 function PagePanel() {
 
 
 
   return (
-    <Container maxWidth={"lg"}>
 
-      
-      <Typography variant="h5" align="center" color="textSecondary" paragraph>
-        <img src={imagem} width={250} alt="nao encontrada" />
-      </Typography>
-      <Typography
-        component="h1"
-        variant="h4"
-        align="center"
-        color="textPrimary"
-        gutterBottom
-      >
-        Painel de Acompanhamento
-      </Typography>
-      <GridPanel/>
+    <Container maxWidth={"lg"}>
+      <br/>
+      <Card sx={{ display: 'flex' }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <CardContent sx={{ flex: '1 0 auto' }}>
+            <Typography component="div" variant="h5">
+              Painel de Acompanhamento de Metas e Ações
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" component="div">
+              Este painel possui a lista de metas institucionais e as ações a serem tomadas para realizar tais ações.  
+            </Typography>
+          </CardContent>
+
+        </Box>
+        <Box >
+          <CardMedia
+            component="img"
+            sx={{ width: 250 }}
+            image={imagem}
+            alt="Logo Proplan"
+          />
+        </Box>
+      </Card>
+
+      <GridPanel />
     </Container>
   );
 }
