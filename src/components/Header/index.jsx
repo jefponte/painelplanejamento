8911cfabfd4124e232953c1c1475a11c6150ae69
Jefferson Uchôa Ponte <jefponte@gmail.com>
2022-10-10 10:44:21 -0300

    Fix header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,11 +5,8 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { AppBar, Toolbar } from "@mui/material";
 import Image from "../../assets/img/bg-topo.png";
 import LogoUNILAB from "../../assets/img/logo-unilab.png";
@@ -32,21 +29,12 @@ const pages = ['Início', 'Sobre', 'Contato'];
 
 const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
   };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
