7700c6435322006bc4c3f108dfecaed28bb7a8ac
Jefferson Uchoa Ponte <jefponte@gmail.com>
2024-05-21 15:25:58 -0300

    Change table

--- src/components/PanelTitle/index.jsx
+++ src/components/PanelTitle/index.jsx
@@ -9,7 +9,7 @@ export default function PanelTitle() {
         <Grid container spacing={2}>
             <Grid item xs={12} md={9}>
                 <Typography component="h4" variant="h4">
-                    Painel de Acompanhamento do Plano de Desenvolvimento (PDI) da UNILAB
+                    Painel de Acompanhamento do Plano de Desenvolvimento Institucional (PDI) da UNILAB
                 </Typography>
                 <Typography variant="subtitle1" color="text.secondary" component="div">
                     Painel gerencial para acompanhamento das metas do PDI (2023-2027) da Unilab
